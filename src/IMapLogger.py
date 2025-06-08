bc83dfadf95389319515376bfce2505ba897248a
Ricardo <Correia.ferreira.ricardo@gmail.com>
2025-06-08 18:54:30 +0200

    html generator finish

--- src/IMapLogger.py
+++ src/IMapLogger.py
@@ -2,5 +2,5 @@ from abc import ABC, abstractmethod
 
 class IMapLogger(ABC):
     @abstractmethod
-    def log(self, map_grid):
+    def log(self, map_grid, title=None):
         pass
--- src/main.py
+++ src/main.py
@@ -1,30 +1,28 @@
 from src.fireGenerator import FireGenerator
-from src.consoleMapLogger import ConsoleMapLogger
+from src.htmlMapLogger import HtmlMapLogger
 from src.mapGenerator import MapGenerator
 from src.bestCut import BestCut
 from src.node import Node
 from copy import deepcopy
 
 def main():
-    # 1. Generate map
+    # 1. Générer la carte
     map_without_cut = MapGenerator(width=10, height=10, percentage_of_trees=50)
     map_without_cut.generate()
 
-    logger = ConsoleMapLogger()
-    print("Carte initiale :")
-    logger.log(map_without_cut)
+    logger = HtmlMapLogger("simulation.html")
+    logger.log(map_without_cut, title="Carte initiale")
 
     map_with_cut = deepcopy(map_without_cut)
 
-    # 3. Map without cut
+    # 2. Simulation sans coupe
     sim1 = FireGenerator(map_without_cut)
     origin = sim1.start_fire()
     sim1.spread()
 
-    print("\nCarte après feu (sans arbre coupé) :")
-    logger.log(map_without_cut)
+    logger.log(map_without_cut, title="Après feu sans arbre coupé")
 
-    # 4. Map with cut
+    # 3. Simulation avec arbre coupé
     if origin:
         best_cut_pos = BestCut.find_best_tree_to_cut(map_with_cut, origin)
         if best_cut_pos:
@@ -35,10 +33,11 @@ def main():
         sim2.start_fire()
         sim2.spread()
 
-        print("\nCarte après feu (avec arbre coupé) :")
-        logger.log(map_with_cut)
+        logger.log(map_with_cut, title="Après feu avec arbre coupé")
     else:
-        print("Pas de feu initial.")
+        logger.log(map_with_cut, title="Aucun feu initial.")
+
+    logger.end()
 
 if __name__ == "__main__":
     main()
